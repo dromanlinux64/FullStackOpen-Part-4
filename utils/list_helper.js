02d72d232605b2e66168c67341052da2cc035e1c
Douglas Roman <droman64@gmail.com>
2025-03-28 12:55:07 -0400

    4.7 Funciones auxiliares y prueba unitarias paso 5

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,3 +1,5 @@
+const {maxBy, keyBy, countBy,map, groupBy }= require('lodash');
+
 const dummy = (blogs) => {
     // ...
     return 1
@@ -22,8 +24,39 @@ const dummy = (blogs) => {
         likes: result.likes
         }
   }
+
+  const mostBlogs = (blogs)=>{
+    if (blogs.length === 0) return {}
+    const result1 = countBy(blogs,"author")
+    //console.log("result1",result1)
+    const result2 = map(result1,(valor,author)=>{
+       return  {"author":author, "blogs":valor}
+    })
+    //console.log("result2",result2)
+    const result3 = maxBy(result2,"blogs")
+    //console.log("result3",result3)
+    return  result3
+  }
+
+  const mostLikes  = (blogs)=>{
+    if (blogs.length === 0) return {}
+    const result1 = groupBy(blogs,"author")
+    //console.log("result1",result1)
+    const result2 = map(result1,(valor,author)=>{
+       const sumLikes =  result1[author].reduce((likes,blog)=>{
+           return likes+blog.likes
+       },0)
+       return  {"author":author, "likes":sumLikes}
+    })
+    //console.log("result2",result2)
+    const result3 = maxBy(result2,"likes")
+    //console.log("result3",result3)
+    return  result3
+  }
   module.exports = {
     dummy,
     totalLikes, 
-    favoriteBlog
+    favoriteBlog,
+    mostBlogs,
+    mostLikes 
   }
